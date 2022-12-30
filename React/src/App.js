dcd626c56c291829604258690881b9c5b2262bd5
Constant-dem <c.rodriguez@dem-soft.com>
2022-12-30 18:16:22 +0100

    added people cards

--- React/src/App.js
+++ React/src/App.js
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function App() {
   const [name, setName] = useState("");
   const [result, setResult] = useState([]);
-
-  useEffect(() => {
-
-  });
 
   async function searchData(e){
     e.preventDefault()
@@ -33,9 +36,32 @@ function App() {
       </form>
       <div>
         {result.map((result) => {
-          return <div key={result.name}>
-            {JSON.stringify(result)}
-            </div>;
+          // return <div key={result.name}>
+          //   {JSON.stringify(result)}
+          //   </div>;
+          return (
+            <Card key={result.name} sx={{ minWidth: 150 }}>
+              <CardContent>
+                <Typography variant="h3" component="div">
+                  Name : {result.name}
+                </Typography>
+                <Typography variant="h5" component="div">
+                  Gender : {result.gender}
+                  <br />
+                  Hair color : {result.hair_color}
+                  <br />
+                  Eyes color : {result.eye_color}
+                  <br />
+                  Skin color : {result.skin_color}
+                  <br />
+                  Height : {result.height}
+                  <br />
+                  Mass : {result.mass}
+                  <br />
+                </Typography>
+              </CardContent>
+            </Card>
+          );
         })}
       </div>
     </div>
